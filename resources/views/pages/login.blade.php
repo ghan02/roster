ec264a2bdae14d5ccc7f920a061c9aa193de89a9
Gagan Janjua <gj1118@gmail.com>
2015-10-03 02:33:25 -0400

    adding changes to the login form.

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,5 +1,41 @@
 @extends('master.master')
 
 @section('content')
-	<p>Welcome to the login page</p>
+	<div class="text-center">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+			    <div class="panel-heading">
+				    Sign in to your account
+				</div>
+				<div class="panel-body">
+				    <form class="form-horizontal" role="form">
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">E-Mail Address</label>
+							<div class="col-md-6">
+								<input type="email" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Password</label>
+							<div class="col-md-6">
+								<input type="password" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-offset-2">
+								<a href="#" class="btn btn-primary">
+									<span class="lnr lnr-checkmark-circle"></span> Login
+								</a>
+								<a class="btn btn-link" href="#">Forgot Your Password?</a>
+
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
