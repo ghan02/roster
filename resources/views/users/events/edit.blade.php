9acb0fac415260860028ef6b30fafdeaae7e3ff2
Gagan Janjua <gj1118@gmail.com>
2015-10-05 02:36:34 -0400

    removed a redundant div

--- resources/views/users/events/edit.blade.php
+++ resources/views/users/events/edit.blade.php
@@ -10,19 +10,19 @@
 @stop
 
 @section('profileContent')
+
 	<div class="row">
-		<div class="row">
-			<div class="well" style="overflow: hidden; line-height: 170%;">
-				<h4>New Event</h4>
-				
-				<small class="text-muted">Use this page to create a new event.</small>
-				<img src="{!!asset('images/event.png')!!}" class="pageImage pull-right" style="margin-top: -30px;" alt="My Events">
-			</div>
-		</div>
-		<div class="row">
-			<form action="/upload" class="dropzone" id="my-awesome-dropzone">
-				@include('users.events.partials._form',['type'=>'edit'])
-			</form>
+		<div class="well" style="overflow: hidden; line-height: 170%;">
+			<h4>New Event</h4>
+			
+			<small class="text-muted">Use this page to create a new event.</small>
+			<img src="{!!asset('images/event.png')!!}" class="pageImage pull-right" style="margin-top: -30px;" alt="My Events">
 		</div>
 	</div>
+	<div class="row">
+		<form action="/upload" class="dropzone" id="my-awesome-dropzone">
+			@include('users.events.partials._form',['type'=>'edit'])
+		</form>
+	</div>
+
 @stop
