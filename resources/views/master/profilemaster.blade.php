c329b5ebc37c55a0fbb8eb869472b0204066b28f
Gagan Janjua <gj1118@gmail.com>
2015-10-09 02:39:38 -0400

    adding more acl stuff

--- resources/views/master/profilemaster.blade.php
+++ resources/views/master/profilemaster.blade.php
@@ -35,8 +35,12 @@
             				{!!HTML::navlink('user.events', '<span class="lnr lnr-rocket"></span> Events')!!}
             				{!!HTML::navlink('user.password', '<span class="lnr lnr-lock"></span> Password')!!}
             				@can('administer')
-            					{!!HTML::navlink('user.password', '<span class="lnr lnr-lock"></span> Create Users')!!}
-
+            					{!!HTML::navlink('user.password', '<span class="lnr lnr-users"></span> Create Users')!!}
+            					{!!HTML::navlink('user.password', '<span class="lnr lnr-tag"></span> Create Projects')!!}
+								
+            				@endcan
+            				@can('roster_manager')
+            					{!!HTML::navlink('user.password', '<span class="lnr lnr-users"></span> Create Users')!!}
             				@endcan
 						</ul>
 					</div>
